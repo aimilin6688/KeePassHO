8ac44a131a929098d6cb1e4f0ee747b2e4cc024f
aimilin <aimilin@yeah.net>
2025-06-15 22:32:28 +0800

    refactor(test): 更新测试框架 - 将 expect.js 替换为 @ohos/hypium，重命名测试文件扩展名，添加测试日志输出，并新增 ArrayBuffer 转换工具函数

--- kdbxweb/src/main/ets/crypto/crypto-engine.ts
+++ kdbxweb/src/main/ets/crypto/crypto-engine.ts
@@ -15,11 +15,12 @@ import { arrayToBuffer, bytesToString, hexToBytes, stringToBuffer } from '../uti
 import { ChaCha20 } from './chacha20';
 import { CryptoJS } from '@ohos/crypto-js';
 import { cryptoFramework } from '@kit.CryptoArchitectureKit';
+import { ByteUtils } from '..';
 
 const EmptySha256 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';
 const EmptySha512 =
-    'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce' +
-        '47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e';
+  'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce' +
+    '47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e';
 
 const rand = cryptoFramework.createRandom();
 
@@ -28,119 +29,119 @@ const rand = cryptoFramework.createRandom();
 const MaxRandomQuota = 65536;
 
 export function sha256(data: ArrayBuffer): Promise<ArrayBuffer> {
-    if (!data.byteLength) {
-        return Promise.resolve(arrayToBuffer(hexToBytes(EmptySha256)));
-    }
-    return new Promise((resolve) => {
-        const sha = CryptoJS.SHA256(bytesToString(data));
-        resolve(stringToBuffer(sha.toString(CryptoJS.enc.Utf8)));
-    });
+  if (!data.byteLength) {
+    return Promise.resolve(arrayToBuffer(hexToBytes(EmptySha256)));
+  }
+  return new Promise((resolve) => {
+    const sha = CryptoJS.SHA256(CryptoJS.lib.WordArray.create(data));
+    resolve(ByteUtils.bytesToBuffer(ByteUtils.hexToBytes(sha.toString())));
+  });
 }
 
 export function sha512(data: ArrayBuffer): Promise<ArrayBuffer> {
-    if (!data.byteLength) {
-        return Promise.resolve(arrayToBuffer(hexToBytes(EmptySha512)));
-    }
-    return new Promise((resolve) => {
-        const sha = CryptoJS.SHA512(bytesToString(data));
-        resolve(stringToBuffer(sha.toString(CryptoJS.enc.Utf8)));
-    });
+  if (!data.byteLength) {
+    return Promise.resolve(arrayToBuffer(hexToBytes(EmptySha512)));
+  }
+  return new Promise((resolve) => {
+    const sha = CryptoJS.SHA512(CryptoJS.lib.WordArray.create(data));
+    resolve(ByteUtils.bytesToBuffer(ByteUtils.hexToBytes(sha.toString())));
+  });
 }
 
 export function hmacSha256(key: ArrayBuffer, data: ArrayBuffer): Promise<ArrayBuffer> {
-    return new Promise((resolve) => {
-        const sha = CryptoJS.HmacSHA256(bytesToString(data), bytesToString(key));
-        resolve(stringToBuffer(sha.toString(CryptoJS.enc.Utf8)));
-    });
+  return new Promise((resolve) => {
+    const sha = CryptoJS.HmacSHA256(CryptoJS.lib.WordArray.create(data), CryptoJS.lib.WordArray.create(key));
+    resolve(ByteUtils.bytesToBuffer(ByteUtils.hexToBytes(sha.toString())));
+  });
 }
 
 export abstract class AesCbc {
-    abstract importKey(key: ArrayBuffer): Promise<void>;
+  abstract importKey(key: ArrayBuffer): Promise<void>;
 
-    abstract encrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer>;
+  abstract encrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer>;
 
-    abstract decrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer>;
+  abstract decrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer>;
 }
 
 class AesCbcNode extends AesCbc {
-    private _key: ArrayBuffer | undefined;
+  private _key: ArrayBuffer | undefined;
 
-    private get key(): ArrayBuffer {
-        if (!this._key) {
-            throw new KdbxError(ErrorCodes.InvalidState, 'no key');
+  private get key(): ArrayBuffer {
+    if (!this._key) {
+      throw new KdbxError(ErrorCodes.InvalidState, 'no key');
+    }
+    return this._key;
+  }
+
+  importKey(key: ArrayBuffer): Promise<void> {
+    this._key = key;
+    return Promise.resolve();
+  }
+
+  encrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer> {
+    return Promise.resolve().then(() => {
+      const encrypted = CryptoJS.AES.encrypt(
+        CryptoJS.lib.WordArray.create(data),
+        CryptoJS.lib.WordArray.create(this.key),
+        {
+          iv: CryptoJS.lib.WordArray.create(iv),
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7
         }
-        return this._key;
-    }
+      );
+      return ByteUtils.wordArrayToArrayBuffer(encrypted.ciphertext);
+    });
+  }
 
-    importKey(key: ArrayBuffer): Promise<void> {
-        this._key = key;
-        return Promise.resolve();
-    }
-
-    encrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer> {
-        return Promise.resolve().then(() => {
-            const cipher = CryptoJS.AES.encrypt(
-                bytesToString(data),
-                bytesToString(this.key),
-                {
-                    iv: CryptoJS.lib.WordArray.create(iv),
-                    mode: CryptoJS.mode.CBC,
-                    padding: CryptoJS.pad.Pkcs7
-                }
-            );
-            return stringToBuffer(cipher.toString());
-        });
-    }
-
-    decrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer> {
-        return Promise.resolve()
-            .then(() => {
-                const cipher = CryptoJS.AES.decrypt(
-                    bytesToString(data),
-                    bytesToString(this.key),
-                    {
-                        iv: CryptoJS.lib.WordArray.create(iv),
-                        mode: CryptoJS.mode.CBC,
-                        padding: CryptoJS.pad.Pkcs7
-                    }
-                );
-                return stringToBuffer(cipher.toString(CryptoJS.enc.Utf8));
-            })
-            .catch(() => {
-                throw new KdbxError(ErrorCodes.InvalidKey, 'invalid key');
-            });
-    }
+  decrypt(data: ArrayBuffer, iv: ArrayBuffer): Promise<ArrayBuffer> {
+    return Promise.resolve()
+      .then(() => {
+        const decrypted = CryptoJS.AES.decrypt(
+          CryptoJS.lib.CipherParams.create({ ciphertext: CryptoJS.lib.WordArray.create(data) }),
+          CryptoJS.lib.WordArray.create(this.key),
+          {
+            iv: CryptoJS.lib.WordArray.create(iv),
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+          }
+        );
+        return ByteUtils.wordArrayToArrayBuffer(decrypted);
+      })
+      .catch((e: Error) => {
+        throw new KdbxError(ErrorCodes.InvalidKey, 'Invalid key');
+      });
+  }
 }
 
 export function createAesCbc(): AesCbc {
-    return new AesCbcNode();
+  return new AesCbcNode();
 }
 
 function safeRandomWeb(len: number): Uint8Array {
-    const randomBytes = new Uint8Array(len);
-    while (len > 0) {
-        let segmentSize = len % MaxRandomQuota;
-        segmentSize = segmentSize > 0 ? segmentSize : MaxRandomQuota;
-        const randomBytesSegment = rand.generateRandomSync(segmentSize).data;
-        len -= segmentSize;
-        randomBytes.set(randomBytesSegment, len);
-    }
-    return randomBytes;
+  const randomBytes = new Uint8Array(len);
+  while (len > 0) {
+    let segmentSize = len % MaxRandomQuota;
+    segmentSize = segmentSize > 0 ? segmentSize : MaxRandomQuota;
+    const randomBytesSegment = rand.generateRandomSync(segmentSize).data;
+    len -= segmentSize;
+    randomBytes.set(randomBytesSegment, len);
+  }
+  return randomBytes;
 }
 
 export function random(len: number): Uint8Array {
-    return safeRandomWeb(len);
+  return safeRandomWeb(len);
 }
 
 export function chacha20(
-    data: ArrayBuffer,
-    key: ArrayBuffer,
-    iv: ArrayBuffer
+  data: ArrayBuffer,
+  key: ArrayBuffer,
+  iv: ArrayBuffer
 ): Promise<ArrayBuffer> {
-    return Promise.resolve().then(() => {
-        const algo = new ChaCha20(new Uint8Array(key), new Uint8Array(iv));
-        return arrayToBuffer(algo.encrypt(new Uint8Array(data)));
-    });
+  return Promise.resolve().then(() => {
+    const algo = new ChaCha20(new Uint8Array(key), new Uint8Array(iv));
+    return arrayToBuffer(algo.encrypt(new Uint8Array(data)));
+  });
 }
 
 export const Argon2TypeArgon2d = 0;
@@ -152,43 +153,43 @@ export type Argon2Type = typeof Argon2TypeArgon2d | typeof Argon2TypeArgon2id;
 export type Argon2Version = 0x10 | 0x13;
 
 export type Argon2Fn = (
-    password: ArrayBuffer,
-    salt: ArrayBuffer,
-    memory: number,
-    iterations: number,
-    length: number,
-    parallelism: number,
-    type: Argon2Type,
-    version: Argon2Version
+  password: ArrayBuffer,
+  salt: ArrayBuffer,
+  memory: number,
+  iterations: number,
+  length: number,
+  parallelism: number,
+  type: Argon2Type,
+  version: Argon2Version
 ) => Promise<ArrayBuffer>;
 
 let argon2impl: Argon2Fn | undefined;
 
 export function argon2(
-    password: ArrayBuffer,
-    salt: ArrayBuffer,
-    memory: number,
-    iterations: number,
-    length: number,
-    parallelism: number,
-    type: Argon2Type,
-    version: Argon2Version
+  password: ArrayBuffer,
+  salt: ArrayBuffer,
+  memory: number,
+  iterations: number,
+  length: number,
+  parallelism: number,
+  type: Argon2Type,
+  version: Argon2Version
 ): Promise<ArrayBuffer> {
-    if (argon2impl) {
-        return argon2impl(
-            password,
-            salt,
-            memory,
-            iterations,
-            length,
-            parallelism,
-            type,
-            version
-        ).then(arrayToBuffer);
-    }
-    return Promise.reject(new KdbxError(ErrorCodes.NotImplemented, 'argon2 not implemented'));
+  if (argon2impl) {
+    return argon2impl(
+      password,
+      salt,
+      memory,
+      iterations,
+      length,
+      parallelism,
+      type,
+      version
+    ).then(arrayToBuffer);
+  }
+  return Promise.reject(new KdbxError(ErrorCodes.NotImplemented, 'argon2 not implemented'));
 }
 
 export function setArgon2Impl(impl: Argon2Fn): void {
-    argon2impl = impl;
+  argon2impl = impl;
 }
--- kdbxweb/src/main/ets/utils/byte-utils.ts
+++ kdbxweb/src/main/ets/utils/byte-utils.ts
@@ -59,6 +59,10 @@ export function bytesToHex(arr: ArrayBufferOrArray): string {
     return str;
 }
 
+export function bytesToBuffer(arr: ArrayBufferOrArray): ArrayBuffer {
+    return arrayToBuffer(arr);
+}
+
 export function arrayToBuffer(arr: ArrayBufferOrArray): ArrayBuffer {
     if (arr instanceof ArrayBuffer) {
         return arr;
@@ -77,4 +81,38 @@ export function stringToBuffer(str: string): ArrayBuffer {
 export function zeroBuffer(arr: ArrayBufferOrArray): void {
     const intArr = arr instanceof ArrayBuffer ? new Uint8Array(arr) : arr;
     intArr.fill(0);
+}
+
+/**
+ * 工具函数：ArrayBuffer 转 CryptoJS WordArray
+ * @param buffer 输入的 ArrayBuffer
+ * @returns CryptoJS WordArray 对象
+ */
+export function arrayBufferToWordArray(buffer: ArrayBuffer): CryptoJS.lib.WordArray {
+    const u8 = new Uint8Array(buffer);
+    const len = u8.length;
+    const words: number[] = [];
+
+    for (let i = 0; i < len; i++) {
+        words[i >>> 2] |= u8[i] << (24 - (i % 4) * 8);
+    }
+
+    return CryptoJS.lib.WordArray.create(words, len);
+}
+
+/**
+ * 工具函数：CryptoJS WordArray 转 ArrayBuffer
+ * @param wordArray 输入的 WordArray
+ * @returns ArrayBuffer 对象
+ */
+export function wordArrayToArrayBuffer(wordArray: CryptoJS.lib.WordArray): ArrayBuffer {
+    const words = wordArray.words;
+    const sigBytes = wordArray.sigBytes;
+    const u8 = new Uint8Array(sigBytes);
+
+    for (let i = 0; i < sigBytes; i++) {
+        u8[i] = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+    }
+
+    return u8.buffer;
 }
