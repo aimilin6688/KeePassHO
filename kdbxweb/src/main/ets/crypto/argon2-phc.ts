031f7b328ce455b5b059f13f428b5e38b83344db
liujunguang1 <liujunguang1@jd.com>
2025-08-09 11:33:41 +0800

    refactor(crypto): 移除 OHOS-RS 实现并优化 PHC 适配 - 删除 argon2-ohos-rs.ts 文件，优化 argon

--- kdbxweb/src/main/ets/crypto/argon2-phc.ts
+++ kdbxweb/src/main/ets/crypto/argon2-phc.ts
@@ -1,5 +1,5 @@
-import { Argon2Type, Argon2Version,Argon2TypeArgon2id } from './crypto-engine';
-import { argon2Hash, HashOption } from '@huozhiniao/argon2';
+import { Argon2Type, Argon2TypeArgon2id, Argon2Version } from './crypto-engine';
+import { argon2Hash } from '@huozhiniao/argon2';
 
 
 export function ohosArgon2(
@@ -12,17 +12,15 @@ export function ohosArgon2(
   type: Argon2Type,
   version: Argon2Version
 ): Promise<ArrayBuffer> {
-  const algorithmType:number = type === Argon2TypeArgon2id ? 2 : 0;
-  const rsVersion:number = version === 0x13 ? 19 : 16;
-
+  const algorithmType: number = type === Argon2TypeArgon2id ? 2 : 0;
   try {
-    const res = argon2Hash(password, salt,  {
+    const res = argon2Hash(password, salt, {
       memoryCost: memory,
       timeCost: iterations,
       hashLength: length,
       parallelism: parallelism,
       argon2Type: algorithmType,
-      argon2Version: rsVersion
+      argon2Version: version
     });
     if (res.result != 'ARGON2_OK') {
       throw new Error(res.result);
