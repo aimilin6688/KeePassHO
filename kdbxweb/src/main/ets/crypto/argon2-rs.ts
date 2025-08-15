02a4252afd450a37dee6aef27a0c48c2119e8561
liujunguang1 <liujunguang1@jd.com>
2025-08-15 19:01:07 +0800

    feat(crypto): 优化 Argon2 哈希日志与事件机制 - 在 OHOS Argon2 哈希计算中增加参数日志输出，便于调试；同时将数据库合并事件名由 DATABASE_MERGE 更改为 DATABASE_RELOAD，保持事件语义一致性，并同步更新相关调用与监听逻辑

--- kdbxweb/src/main/ets/crypto/argon2-rs.ts
+++ kdbxweb/src/main/ets/crypto/argon2-rs.ts
@@ -1,6 +1,9 @@
 import { Argon2Type, Argon2Version,Argon2TypeArgon2id } from './crypto-engine';
 import {Algorithm,Version, hashRaw } from '@ohos-rs/argon2'
 
+import { hilog } from '@kit.PerformanceAnalysisKit';
+const DOMAIN = 0x0000;
+const TAG = 'ohosArgon2';
 
 export function ohosArgon2(
     password: ArrayBuffer,
@@ -15,7 +18,7 @@ export function ohosArgon2(
     const algorithmType:Algorithm = type === Argon2TypeArgon2id ? Algorithm.Argon2id : Algorithm.Argon2d;
     const rsVersion:Version = version === 0x13 ? Version.V0x13 : Version.V0x10;
     try {
-        const res = hashRaw(password, {
+        const param = {
             memoryCost: memory,
             timeCost: iterations,
             outputLen: length,
@@ -23,7 +26,9 @@ export function ohosArgon2(
             algorithm: algorithmType,
             version: rsVersion,
             salt: salt
-        });
+        };
+        hilog.info(DOMAIN, TAG, 'ohosArgon2 param: ' + JSON.stringify(param));
+        const res = hashRaw(password, param);
         return Promise.resolve(res);
     } catch (e) {
         return Promise.reject(e);
